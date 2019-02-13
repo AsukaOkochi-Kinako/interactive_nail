5a247db816a0398be7b7c9cc40e7ae73fadc0050
asukaokochi <x17018xx@gmail.com>
2019-02-13 23:50:40 +0900

    [add]初めてのコミット

---
+++ script.js
@@ -0,0 +1,18 @@
+function getMousePosition(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function draw() {
+    var canvas = document.getElementById('SimpleCanvas');
+    var context = canvas.getContext('2d');
+
+    canvas.addEventListener('mousemove', function (evt) {
+        var mousePos = getMousePosition(canvas, evt);
+        //        var message = 'Mouse position X:' + mousePos.x + ', Y:' + mousePos.y;
+        console.log(canvas, message);
+    }, false);
+}
